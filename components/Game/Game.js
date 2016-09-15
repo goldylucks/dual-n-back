f4fb581a01272a5db7dfc76c7a3cf87b3d209b0d
Adam Goldman <adamgoldman7@gmail.com>
2016-09-15 11:07:19 +0200

    POC

---
+++ components/Game/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Game extends Component {
+
+  render () {
+    return (
+      <View>
+        <Text>Game goes here!</Text>
+      </View>
+      );
+  }
+
+}
---
+++ routes/routes.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Navigator } from 'react-native';
+
+import Home from '../components/Home/Home';
+import Game from '../components/Game/Game';
+
+export default class Routes extends Component {
+
+  initialRoute = { title: 'home', index: 0 }
+
+  render () {
+    return (
+      <Navigator
+        initialRoute={ this.initialRoute }
+        renderScene={ this.renderScene }
+      />
+    );
+  }
+
+  renderScene = (route, navigator) => {
+    if (route.index === 0) {
+      return (
+        <Home routeToGame={ this.routeToGame.bind(this, route, navigator) } />
+      );
+    }
+
+    if (route.index === 1) {
+      return (
+        <Game />
+      );
+    }
+
+  }
+
+  routeToGame (route, navigator) {
+    navigator.push({
+      title: 'Game title',
+      index: 1
+    })
+  }
+
+}
