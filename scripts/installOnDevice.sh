0fe50c08bec3e0e8677eb6f31f753d3ed06112fc
Adam Goldman <adamgoldman7@gmail.com>
2016-11-08 10:21:33 +0200

    (scripts): add dev option to install on device script

--- scripts/installOnDevice.sh
+++ scripts/installOnDevice.sh
@@ -3,8 +3,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ..
 node ./scripts/configGradlew $1 # pass -p as first argument to set to production
-cd android
-./gradlew assembleRelease
-cd ..
-react-native run-android --variant=release
+
+if [ "$1" = '-p' ]; then
+  cd android
+  ./gradlew assembleRelease
+  cd ..
+fi
+
+if [ "$1" = '-p' ]; then
+  react-native run-android --variant=release
+else
+  react-native run-android
+fi
+
 exit 0
