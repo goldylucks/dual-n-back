8264ea58ea8ac33c404c4457203b40c92c6102a3
Adam Goldman <adamgoldman7@gmail.com>
2016-11-09 04:27:33 +0200

    (UT): separate web and shared from native

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -4,3 +4,4 @@ touch .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 echo npm run lint >> .git/hooks/pre-commit
 echo npm test >> .git/hooks/pre-commit
+echo npm run test:native >> .git/hooks/pre-commit
--- test/setup.spec.js
+++ test/setup.spec.js
@@ -1,3 +1,11 @@
+beforeEach('global setup', () => {
+  global.dispatch.reset()
+  global.setTimeout.reset()
+  global.setInterval.reset()
+  global.clearInterval.reset()
+  global.clearTimeout.reset()
+})
+
 afterEach('global teardown', () => {
   global.dispatch.reset()
   global.setTimeout.reset()
