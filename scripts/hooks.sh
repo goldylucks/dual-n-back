9c758a76aaf5a4b4e62c16d5cd355fa3496e21c8
Adam Goldman <adamgoldman7@gmail.com>
2016-11-20 12:52:35 +0200

    (scripts): update hooks

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+set -e # stop on error
+
 rm -f .git/hooks/pre-commit
 touch .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
+echo set -e >> .git/hooks/pre-commit
 echo npm run lint >> .git/hooks/pre-commit
 echo npm test >> .git/hooks/pre-commit
 # commented out until react native and mocha react native
 # will support react 15.4
 # echo npm run test:native >> .git/hooks/pre-commit
+
+echo pre-commit hooks installed successfully:
+echo npm run lint
+echo npm test
+exit 0
