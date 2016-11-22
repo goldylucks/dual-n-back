d32b7f442a5e536fb95b0f8cd821f6e32cf73717
Adam Goldman <adamgoldman7@gmail.com>
2016-11-22 20:21:07 +0200

    (ci): update deploy gh script

--- scripts/deploy-to-gh-pages-ci.sh
+++ scripts/deploy-to-gh-pages-ci.sh
@@ -2,6 +2,9 @@
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
+# only deploy merges of master
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+
 # config git
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
@@ -13,5 +16,3 @@ git init
 git add .
 git commit -m "Deploy to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
