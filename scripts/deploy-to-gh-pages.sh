488f613fc3869ed42ff5093a28bb61c0a2dbba6c
Adam Goldman <adamgoldman7@gmail.com>
2016-11-22 18:04:56 +0200

    (script): add deploy to gh-pages

---
+++ scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e # stop on error
+
+echo bulid client ...
+npm run build:client
+echo check out branch gh-pages ...
+git checkout -b gh-pages
+echo add client-dist folder
+git add -f client-dist
+echo commit changes
+git commit -m "deploy to gh-pages"
+echo push to remote gh-pages
+git push origin `git subtree split --prefix client-dist`:gh-pages --force
+echo checkout branch master
+git checkout master
+echo delete branch gh-pages
+git branch -D gh-pages
+echo All done!
