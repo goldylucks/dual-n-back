808b014e8a41e952cab41fbcaa5ba0d53f62935a
Adam Goldman <adamgoldman7@gmail.com>
2016-11-22 20:33:39 +0200

    (CI): update GH  token

--- scripts/deploy-to-gh-pages-ci.sh
+++ scripts/deploy-to-gh-pages-ci.sh
@@ -16,3 +16,4 @@ git init
 git add .
 git commit -m "Deploy to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
+echo "successfully deployed to ${TRAVIS_REPO_SLUG}"
