b7d4ba8b6eacbfa421b465bb3fa49175fb32092f
Adam Goldman <adamgoldman7@gmail.com>
2016-11-22 21:40:19 +0200

    (CI): update gh pages script (TEST)

--- shared/utils/index.js
+++ shared/utils/index.js
@@ -2,6 +2,8 @@ export function capitalize (word) {
   return word[0].toUpperCase() + word.slice(1).toLowerCase()
 }
 
+export const TODO_DELETE_ME = 5
+
 export const renderIf = predicate => elem => {
   return predicate ? elem : null
 }
