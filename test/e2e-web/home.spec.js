8b348e5ef9added5947573cce0df701f71338423
Adam Goldman <adamgoldman7@gmail.com>
2016-11-24 22:09:00 +0200

    rename(e2e/home): HomePage -> HomeContainer

--- test/e2e-web/home.spec.js
+++ test/e2e-web/home.spec.js
@@ -8,7 +8,6 @@ module.exports = {
   before (client) {
     client
       .url(url)
-      // set LS with scores to test syncing state from LS
       .execute('localStorage.clear()')
       .waitForElementVisible('body', 5000)
   },
@@ -18,79 +17,79 @@ module.exports = {
   },
 
   'Assert defaults modes' (client) {
-    client.expect.element(el('HomePage-modePosition')).to.be.visible
-    client.expect.element(el('HomePage-modePosition')).to.have.attribute('class').contains('active')
-    client.expect.element(el('HomePage-modeAudio')).to.be.visible
-    client.expect.element(el('HomePage-modeAudio')).to.not.have.attribute('class').contains('active')
-    client.expect.element(el('HomePage-modeColor')).to.be.visible
-    client.expect.element(el('HomePage-modeColor')).to.have.attribute('class').contains('active')
+    client.expect.element(el('HomeContainer-modePosition')).to.be.visible
+    client.expect.element(el('HomeContainer-modePosition')).to.have.attribute('class').contains('active')
+    client.expect.element(el('HomeContainer-modeAudio')).to.be.visible
+    client.expect.element(el('HomeContainer-modeAudio')).to.not.have.attribute('class').contains('active')
+    client.expect.element(el('HomeContainer-modeColor')).to.be.visible
+    client.expect.element(el('HomeContainer-modeColor')).to.have.attribute('class').contains('active')
   },
 
   'Assert toggling modes on click' (client) {
-    client.click(el('HomePage-modePosition'))
-    client.expect.element(el('HomePage-modePosition')).to.not.have.attribute('class').contains('active')
-    client.click(el('HomePage-modePosition'))
-    client.expect.element(el('HomePage-modePosition')).to.have.attribute('class').contains('active')
-    client.click(el('HomePage-modeAudio'))
-    client.expect.element(el('HomePage-modeAudio')).to.have.attribute('class').contains('active')
-    client.click(el('HomePage-modeAudio'))
-    client.expect.element(el('HomePage-modeAudio')).to.not.have.attribute('class').contains('active')
-    client.click(el('HomePage-modeColor'))
-    client.expect.element(el('HomePage-modeColor')).to.not.have.attribute('class').contains('active')
-    client.click(el('HomePage-modeColor'))
-    client.expect.element(el('HomePage-modeColor')).to.have.attribute('class').contains('active')
+    client.click(el('HomeContainer-modePosition'))
+    client.expect.element(el('HomeContainer-modePosition')).to.not.have.attribute('class').contains('active')
+    client.click(el('HomeContainer-modePosition'))
+    client.expect.element(el('HomeContainer-modePosition')).to.have.attribute('class').contains('active')
+    client.click(el('HomeContainer-modeAudio'))
+    client.expect.element(el('HomeContainer-modeAudio')).to.have.attribute('class').contains('active')
+    client.click(el('HomeContainer-modeAudio'))
+    client.expect.element(el('HomeContainer-modeAudio')).to.not.have.attribute('class').contains('active')
+    client.click(el('HomeContainer-modeColor'))
+    client.expect.element(el('HomeContainer-modeColor')).to.not.have.attribute('class').contains('active')
+    client.click(el('HomeContainer-modeColor'))
+    client.expect.element(el('HomeContainer-modeColor')).to.have.attribute('class').contains('active')
   },
 
   'Assert alert when trying to toggle last active mode' (client) {
-    client.click(el('HomePage-modePosition'))
-    client.click(el('HomePage-modeColor'))
+    client.click(el('HomeContainer-modePosition'))
+    client.click(el('HomeContainer-modeColor'))
     client.acceptAlert()
-    client.expect.element(el('HomePage-modeColor')).to.have.attribute('class').contains('active')
+    client.expect.element(el('HomeContainer-modeColor')).to.have.attribute('class').contains('active')
   },
 
   'Assert default nBack' (client) {
-    client.expect.element(el('HomePage-nBack')).text.to.equal('2')
+    client.expect.element(el('HomeContainer-nBack')).text.to.equal('2')
   },
 
   'Assert decrement nBack' (client) {
-    client.click(el('HomePage-nBackDecrement'))
-    client.expect.element(el('HomePage-nBack')).text.to.equal('1')
+    client.click(el('HomeContainer-nBackDecrement'))
+    client.expect.element(el('HomeContainer-nBack')).text.to.equal('1')
     // can't go below 1
-    client.click(el('HomePage-nBackDecrement'))
-    client.expect.element(el('HomePage-nBack')).text.to.equal('1')
+    client.click(el('HomeContainer-nBackDecrement'))
+    client.expect.element(el('HomeContainer-nBack')).text.to.equal('1')
   },
 
   'Assert increment nBack' (client) {
-    client.click(el('HomePage-nBackIncrement'))
-    client.click(el('HomePage-nBackIncrement'))
-    client.expect.element(el('HomePage-nBack')).text.to.equal('3')
+    client.click(el('HomeContainer-nBackIncrement'))
+    client.click(el('HomeContainer-nBackIncrement'))
+    client.expect.element(el('HomeContainer-nBack')).text.to.equal('3')
   },
 
   'Assert default speed' (client) {
-    client.expect.element(el('HomePage-speed')).text.to.equal('2000')
+    client.expect.element(el('HomeContainer-speed')).text.to.equal('2000')
   },
 
   'Assert decrement speed' (client) {
-    client.click(el('HomePage-speedDecrement'))
-    client.expect.element(el('HomePage-speed')).text.to.equal('1900')
+    client.click(el('HomeContainer-speedDecrement'))
+    client.expect.element(el('HomeContainer-speed')).text.to.equal('1900')
     // can't go below 100
-    _.times(21, () => client.click(el('HomePage-speedDecrement')))
-    client.expect.element(el('HomePage-speed')).text.to.equal('100')
+    _.times(21, () => client.click(el('HomeContainer-speedDecrement')))
+    client.expect.element(el('HomeContainer-speed')).text.to.equal('100')
   },
 
   'Assert increment speed' (client) {
-    client.click(el('HomePage-speedIncrement'))
-    client.click(el('HomePage-speedIncrement'))
-    client.expect.element(el('HomePage-speed')).text.to.equal('300')
+    client.click(el('HomeContainer-speedIncrement'))
+    client.click(el('HomeContainer-speedIncrement'))
+    client.expect.element(el('HomeContainer-speed')).text.to.equal('300')
   },
 
   'Assert bestScore when no previous games' (client) {
-    client.expect.element(el('HomePage-bestScore')).text.to.contain('0')
+    client.expect.element(el('HomeContainer-bestScore')).text.to.contain('0')
   },
 
   'Assert routing to auth when user not logged in' (client) {
-    client.expect.element(el('HomePage-routeToAuth')).to.be.visible
-    client.click(el('HomePage-routeToAuth'))
+    client.expect.element(el('HomeContainer-routeToAuth')).to.be.visible
+    client.click(el('HomeContainer-routeToAuth'))
     client.assert.urlContains('auth')
     client
       .url(url)
@@ -98,8 +97,8 @@ module.exports = {
   },
 
   'Assert routing to play screen' (client) {
-    client.expect.element(el('HomePage-routeToPlay')).to.be.visible
-    client.click(el('HomePage-routeToPlay'))
+    client.expect.element(el('HomeContainer-routeToPlay')).to.be.visible
+    client.click(el('HomeContainer-routeToPlay'))
     client.assert.urlContains('play')
     client
       .url(url)
@@ -110,33 +109,34 @@ module.exports = {
     client
       .execute('localStorage.setItem("gameConfig", JSON.stringify({nBack: 6, speed: 200, modes:{ audio: true, color: true, position: false }}))')
       .execute('store.dispatch({ type: "init app" })')
-    client.expect.element(el('HomePage-nBack')).text.to.equal('6')
-    client.expect.element(el('HomePage-speed')).text.to.equal('200')
-    client.expect.element(el('HomePage-modePosition')).to.not.have.attribute('class').contains('active')
-    client.expect.element(el('HomePage-modeAudio')).to.have.attribute('class').contains('active')
-    client.expect.element(el('HomePage-modeColor')).to.have.attribute('class').contains('active')
+    client.expect.element(el('HomeContainer-nBack')).text.to.equal('6')
+    client.expect.element(el('HomeContainer-speed')).text.to.equal('200')
+    client.expect.element(el('HomeContainer-modePosition')).to.not.have.attribute('class').contains('active')
+    client.expect.element(el('HomeContainer-modeAudio')).to.have.attribute('class').contains('active')
+    client.expect.element(el('HomeContainer-modeColor')).to.have.attribute('class').contains('active')
   },
 
   'Assert loading scores from LS' (client) {
     client
       .execute('localStorage.setItem("bestScores", JSON.stringify({audiocolor6: 7}))')
       .execute('store.dispatch({ type: "init app" })')
-    client.expect.element(el('HomePage-bestScore')).text.to.contain('7')
-    client.click(el('HomePage-nBackIncrement'))
-    client.expect.element(el('HomePage-bestScore')).text.to.contain('0')
+    client.expect.element(el('HomeContainer-bestScore')).text.to.contain('7')
+    client.click(el('HomeContainer-nBackIncrement'))
+    client.expect.element(el('HomeContainer-bestScore')).text.to.contain('0')
   },
 
   'Assert no auth link when user is logged in' (client) {
     client
       .execute('localStorage.setItem("user", JSON.stringify({name: "John Wayne"}))')
       .execute('store.dispatch({ type: "init app" })')
-    client.expect.element(el('HomePage-routeToAuth')).to.not.be.present
+    client.expect.element(el('HomeContainer-routeToAuth')).to.not.be.present
     client
       .url(url)
       .waitForElementVisible('body', 5000)
   },
 
   after (client) {
+    client.execute('localStorage.clear()')
     client.end()
   },
 }
