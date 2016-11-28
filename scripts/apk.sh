606c9c20f079e4b12abcbb57550a45943b714e85
Adam Goldman <adamgoldman7@gmail.com>
2016-11-28 10:09:16 +0200

    update apk script

--- scripts/apk.sh
+++ scripts/apk.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -e # stop on error
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ..
+node ./scripts/configGradlew -p # pass -p as first argument to set to production
+
 cd android
 ./gradlew assembleRelease
 cd ..
+
+echo successfully generated apk
+
+exit 0
