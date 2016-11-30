def390722de4c4565cb3d465ba01510ac08c4fc8
Adam Goldman <adamgoldman7@gmail.com>
2016-11-30 23:58:20 +0200

    (CI): run tests without sauce (2)

---
+++ scripts/e2e-web-travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e # stop on error
+
+# fire up xvfb on port :99.0
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+# set the xvfb screen size to 1280x1024x16
+/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+
+npm run start:e2e & sleep 20 && npm run e2e:web
