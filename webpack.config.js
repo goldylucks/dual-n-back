aaa2e2bc8801f913066f864cff5542cb32448f38
Adam Goldman <adamgoldman7@gmail.com>
2017-01-25 11:27:07 +0200

    fix(chore): add HMR entries to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   entry: (function () {
     const entries = []
     if (isDev) {
-      entries.concat([
+      entries.push(
         'react-hot-loader/patch',
         // activate HMR for React
 
@@ -24,10 +24,10 @@ module.exports = {
         // bundle the client for webpack-dev-server
         // and connect to the provided endpoint
 
-        'webpack/hot/only-dev-server',
+        'webpack/hot/only-dev-server'
         // bundle the client for hot reloading
         // only- means to only hot reload for successful updates
-      ])
+      )
     }
     return entries.concat('./index.js')
   })(),
